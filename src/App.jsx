2ad6034c9f7b3187167cd9ade3a55fbe456389c3
tk <trishank01@gmail.com>
2023-09-11 20:04:49 +0530

    getting started

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Footer from './compenets/Footer'
+import Navbar from './compenets/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+     <main>
+    <Navbar/>
+    <Footer/>
+     </main>
+    </>
+  )
+}
+
+export default App
